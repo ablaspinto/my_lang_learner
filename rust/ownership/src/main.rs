8b7c6fa71aa88e84d41aa45583f6a6245f783ee3
ablaspinto <ablaspinto@gmail.com>
2024-04-27 21:40:46 -0400

    enums and structs

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -4,7 +4,7 @@ fn caclulate_length(s: &mut String) -> usize {
 }
 fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate(){
+    for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return &s[0..i];
         }
@@ -12,13 +12,32 @@ fn first_word(s: &str) -> &str {
     &s[..]
 }
 
-fn main() {
-    let mut str: String = String::from("hello world");
-    let hello: &String = &str[0..5];
-    let world: &String = &str[6..];
-    let len: usize = caclulate_length(&mut str);
-    println!("The string: {} and the length is: {}", str, len);
-    let len = first_word(&str);
-    
-    println!("{}",len);
+enum IpAddrKind{
+    V4,
+    V6,
 }
+
+struct IpAddr{
+    kind: IpAddrKind,
+    address: String,
+}  
+
+fn main() {
+    let mut s: String = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..];
+    let len: usize = caclulate_length(&mut s);
+    println!("The string: {} and the length is: {}", s, len);
+    let len = first_word(&s);
+    let four = IpAddrKind::V4;
+    let six =IpAddrKind::V6;
+    let localhost = IpAddr{
+        kind: IpAddrKind::V4,
+        address = String::from("127.0.0.1");
+
+    }
+
+
+    println!("{}", len);
+}
+
